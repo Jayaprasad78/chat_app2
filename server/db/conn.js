658437abd342e64b8f396a34c79bbed2911f2ed7
JAYAPRASAD B <127640948+Jayaprasad78@users.noreply.github.com>
2024-01-26 16:00:21 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,1 +1,8 @@
-
+const mongoose = require('mongoose');
+const db=process.env.DATABASE;
+mongoose.connect(db,{  writeConcern: { w: 'majority' }}).then(()=>{
+    console.log('connected to database');
+}).catch((err)=>{
+    console.log('error connecting to database');
+    console.log(err.message);
+})
